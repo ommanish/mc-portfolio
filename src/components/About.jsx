4edb0f7e32f453f3b3132666cadc34800d99ef35
Manish <125258789+ommanish@users.noreply.github.com>
2023-03-05 10:02:52 -0800

    About content update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,12 +17,24 @@ const About = () => {
             <p>Hi. I'm Manish, nice to meet you. Please take a look around.</p>
           </div>
           <div>
-            <p>
+            {/* <p>
               I am passionate about building excellent software that improves
               the lives of those around me. I specialize in creating software
               for clients ranging from individuals and small-businesses all the
               way to large enterprise corporations. What would you do if you had
               a software expert available at your fingertips?
+            </p> */}
+            <p>
+              Well, that's exactly what you get with me! As a skilled frontend
+              developer, you can count on me to create engaging and
+              user-friendly interfaces for your web applications. But what sets
+              me apart is my ability to learn quickly and adapt to new
+              technologies and programming languages. Whether it's mastering a
+              new framework or experimenting with cutting-edge design tools, I'm
+              always eager to expand my skillset and take on new challenges.
+              With my passion for frontend development and my natural curiosity,
+              I'm confident that I can help you build software that not only
+              meets your needs but exceeds your expectations.
             </p>
           </div>
         </div>
