0dcd97dbd13226782190a4f810ee3b5d4a00df2a
Manish <125258789+ommanish@users.noreply.github.com>
2023-09-11 08:47:12 -0700

    linkedin url update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -105,7 +105,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="https://www.linkedin.com/in/mani0981/"
+              href="https://www.linkedin.com/in/manishchawla09/"
             >
               LinkedIn
               <FaLinkedin size={30} />
