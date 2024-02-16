d6eea226360dde2327741415da85f92f7c7aadff
Manish <125258789+ommanish@users.noreply.github.com>
2024-02-15 16:21:48 -0800

    updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <div name="home" className="w-full h-screen bg-[#0a192f]">
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p className="text-pink-600">Hi, my name is</p>
+        <span className="text-pink-600">Hi, my name is</span>
         <h1 className="text-3xl sm:text-5xl font-bold text-[#ccd6f6]">
           Manish Chawla
         </h1>
